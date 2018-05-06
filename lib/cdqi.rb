7e4b334d94fdf89aa71bf075ef022ab3c5622c19
Gregory Higley <code@revolucent.net>
2018-05-06 15:56:53 -0400

    Versioning

--- lib/cdqi.rb
+++ lib/cdqi.rb
@@ -1,8 +1,2 @@
 require "cdqi/version"
 
-module CDQI
-  # Your code goes here...
-  def self.foo
-    puts 'ok'
-  end
-end                  
--- lib/cdqi/version.rb
+++ lib/cdqi/version.rb
@@ -1,3 +1,3 @@
 module CDQI
-  VERSION = "0.1.0"
+  VERSION = "7.0.0"
 end
